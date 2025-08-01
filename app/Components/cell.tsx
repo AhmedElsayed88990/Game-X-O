eca38d9af74d994d3a29814032b3382b796e74a1
AhmedElsayed88990 <sayed22700@gmail.com>
2025-08-01 18:11:43 +0300

    أول رفع للمشروع

---
+++ app/Components/cell.tsx
@@ -0,0 +1,46 @@
+import { Dispatch , SetStateAction } from "react";
+
+type CellProps ={
+    id: number;
+    go: string;
+    setGo: Dispatch<SetStateAction<string>>;
+    cells: string[];
+    setCells: Dispatch<SetStateAction<string[]>>;
+    cell?: string; 
+    winningMessage: string;
+}
+
+const cell = ({go , setGo , id , cells , setCells , cell , winningMessage}:CellProps) => {
+
+    const handleclick = (e) => {
+        if (winningMessage) return;
+        const notTaken = !cells[id]
+        
+        if (notTaken) {
+            
+            if (go === "O" ){
+            handleCellChange("O")
+            setGo("X");
+        }else if (go === "X") {
+            handleCellChange("X")
+            setGo("O");
+        }
+
+        }
+        
+    }
+
+    const handleCellChange = (cellToChange: string) => {
+        let copyCells = [...cells];
+        copyCells[id] = cellToChange;
+        setCells(copyCells);
+
+    }
+    return (
+        <div className="square" onClick={handleclick}>
+            <div className={cell}>{cell ?  (cell === "O" ? "O" : "X") : "" }</div>
+        </div>
+    )
+}
+
+export default cell;
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,76 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Cell from "./Components/cell";
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+const [go , setGo] = useState("O");
+const [winningMessage, setWinningMessage] = useState("");
+
+useEffect(() => {
+  winningCombinations.forEach((combination) => {
+    const circleWins = combination.every((cell) => cells[cell] === "O");
+    const crossWins = combination.every((cell) => cells[cell] === "X");
+    
+    if (circleWins){
+      setWinningMessage("O has won!");
+      
+    }else if (crossWins) {
+      setWinningMessage("X has won!");
+    }
+  })
+}, [cells]);
+
+useEffect(() => {
+  if (cells.every(cell => cell !== "") && !winningMessage) {
+    setWinningMessage("It's a draw!");
+
+  }
+}, [cells , winningMessage])
+
+  return (
+    <>
+    <div className="container">
+
+      <div className="gameboard">
+        {cells.map((cell, index) => (
+          <Cell id={index}
+          go={go}
+          setGo={setGo}
+          key={index}
+          cells={cells}
+          setCells={setCells}
+          cell={cell}
+          winningMessage={winningMessage}
+
+                />
+        ) )}
+      </div>
+      
+      <div>
+        {winningMessage}
+      </div>
+
+      { ! winningMessage && <div>
+        {`it's now ${go} turn`}
+      </div> }
+
     </div>
+    </>
   );
 }
+
+
